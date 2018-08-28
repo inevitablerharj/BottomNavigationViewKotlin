2c7f78dfff824a0e4c1ce3bbd743ee718250a53b
Raji Oladayo <oladayo.raji@techadvance.ng>
2018-08-28 10:17:09 +0100

    Create fragment for each of the bottom menu, Also create a function in the main activity to show the respective fragment when the menu is clicked

--- app/src/main/java/rharj/com/bottomnavigationsample/activity/MainActivity.kt
+++ app/src/main/java/rharj/com/bottomnavigationsample/activity/MainActivity.kt
@@ -2,24 +2,35 @@ package rharj.com.bottomnavigationsample.activity
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import rharj.com.bottomnavigationsample.R
+import rharj.com.bottomnavigationsample.fragment.DashboardFragment
+import rharj.com.bottomnavigationsample.fragment.HomeFragment
+import rharj.com.bottomnavigationsample.fragment.NotificationFragment
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                //message.setText(R.string.title_home)
+                val homFrag = HomeFragment()
+                loadFragment(homFrag)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+               // message.setText(R.string.title_dashboard)
+                val dashFrag = DashboardFragment()
+                loadFragment(dashFrag)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+               // message.setText(R.string.title_notifications)
+                val notificationFrag = NotificationFragment()
+                loadFragment(notificationFrag)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -31,5 +42,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        //loads the first fragment on launch
+        val homFragment = HomeFragment()
+        loadFragment(homFragment)
     }
+
+
+     fun loadFragment(fragment: Fragment) {
+         // load fragment
+         val frgamentTransaction : FragmentTransaction  = getSupportFragmentManager().beginTransaction()
+         frgamentTransaction.replace(R.id.fragment_container, fragment)
+         frgamentTransaction.addToBackStack(null)
+         frgamentTransaction.commit()
+     }
 }
--- app/src/main/java/rharj/com/bottomnavigationsample/fragment/HomeFragment.kt
+++ app/src/main/java/rharj/com/bottomnavigationsample/fragment/HomeFragment.kt
@@ -1,2 +1,22 @@
 package rharj.com.bottomnavigationsample.fragment
 
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import rharj.com.bottomnavigationsample.R
+
+class HomeFragment : Fragment(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Get the custom view for this fragment layout
+        val view = inflater!!.inflate(R.layout.layout_home,container,false)
+
+        return view
+    }
+}
